5007b56269911e59f5092a517c6065c1c6cf36d4
0xVedansh <vedansh83138@gmail.com>
2023-11-16 02:15:18 +0530

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { fetchFROMAPI } from '../utils/fetchFROMAPI';
+
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+    useEffect(() => {
+      fetchFROMAPI(`search?part=snippet&q=${selectedCategory}`);
+    }, [selectedCategory]);
+
   return (
-    <div>
-      Feed
-    </div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2024 Watchify
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
   )
 }
 
